2833c99d472379fb890ea1d6e741b7795b0dc3ab
neylemkeunesp <118134544+neylemkeunesp@users.noreply.github.com>
2022-12-23 18:21:05 -0300

    Incluindo pyttsx3

---
+++ voz.py
@@ -0,0 +1,6 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.setProperty('voice', 'com.apple.eloquence.pt-BR.Luciana')
+engine.say('Pedro viu a uva')
+engine.save_to_file('Pedro viu a uva. Joana viu a pera.','test.wav')
+engine.runAndWait()
